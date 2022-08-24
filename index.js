b1d3f80e656bf5b15e36c0c47fa2af6b38367a7e
mccafee025 <ademaamanka@gmail.com>
2022-08-24 21:31:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+const scuberHq = 42;
+function distanceFromHqInBlocks(street){
+    if ( street > scuberHq ){
+        return street - scuberHq
+    }
+    else if (street < scuberHq){
+        return scuberHq - street
+    }
+}
+function distanceFromHqInFeet(street){
+    return distanceFromHqInBlocks(street)*264
+}
+function distanceTravelledInFeet(start, finish){
+    if (start < finish){
+        return  (finish - start) * 264; 
+    }
+    else if (finish < start){
+        return (start - finish)* 264;
+    }
+}
+function calculatesFarePrice(start, finish){
+    if (distanceTravelledInFeet(start, finish) <= 400){
+        return 0;
+    }
+    else if (distanceTravelledInFeet(start, finish) > 2500){
+        return 'cannot travel that far';
+    }
+    else if (distanceTravelledInFeet(start, finish) > 2000){
+        return 25
+    }
+    else if (distanceTravelledInFeet(start, finish) > 400){
+        return 0.02 * (distanceTravelledInFeet(start, finish) - 400);
+}
+}
